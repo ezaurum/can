9fd14177ba429332eacb6d821f0875057c56796a
조석규 <ezaurum@gmail.com>
2022-09-18 18:31:38 +0900

    feat: Add expire hander

--- repository_test.go
+++ repository_test.go
@@ -2,6 +2,7 @@ package can
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -51,8 +52,9 @@ func TestSet(t *testing.T) {
 		Addr: "localhost:13789",
 	})
 	repository := New(client, &TestMarshaler{})
+	id := "test1"
 	ts := TestSession{
-		ID: "test",
+		ID: id,
 	}
 	assert.NoError(t, repository.Save(ts))
 }
@@ -62,15 +64,16 @@ func TestGet(t *testing.T) {
 		Addr: "localhost:13789",
 	})
 	repository := New(client, &TestMarshaler{})
+	id := "test-get"
 	ts := TestSession{
-		ID:                "test",
+		ID:                id,
 		ExpiresInDuration: time.Minute,
 	}
 	assert.NoError(t, repository.Save(ts))
-	load, err := repository.Load("test")
+	load, err := repository.Load(id)
 	assert.NoError(t, err)
 	assert.NotNil(t, load)
-	assert.Equal(t, "test", load.Key())
+	assert.Equal(t, id, load.Key())
 	assert.Equal(t, time.Minute, load.ExpiresIn())
 }
 
@@ -89,4 +92,48 @@ func TestGroupGet(t *testing.T) {
 	assert.NotNil(t, load)
 	assert.Equal(t, "test", load.Key())
 	assert.Equal(t, time.Minute, load.ExpiresIn())
+}
+
+func TestExpire(t *testing.T) {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:13789",
+	})
+	repository := New(client, &TestMarshaler{})
+	id := "test-expire"
+	ts := TestSession{
+		ID:                id,
+		ExpiresInDuration: time.Second * 2,
+	}
+	expired := false
+	repository.OnExpired(func(key string) {
+		fmt.Println("id", key)
+		if key == id {
+			expired = true
+		}
+	})
+	assert.NoError(t, repository.Save(ts))
+	load, err := repository.Load(id)
+	assert.NoError(t, err)
+	assert.NotNil(t, load)
+	assert.Equal(t, id, load.Key())
+	assert.Equal(t, time.Second*2, load.ExpiresIn())
+
+	time.Sleep(time.Second * 1)
+	_ = repository.Save(TestSession{
+		ID:                id,
+		ExpiresInDuration: time.Second * 2,
+	})
+	time.Sleep(time.Second * 1)
+	load0, err0 := repository.Load(id)
+	assert.NoError(t, err0)
+	assert.NotNil(t, load0)
+	assert.Equal(t, id, load0.Key())
+	assert.Equal(t, time.Second*2, load0.ExpiresIn())
+	time.Sleep(time.Second * 3)
+
+	_, err1 := repository.Load(id)
+	assert.Error(t, err1)
+
+	assert.Equal(t, true, expired)
+	time.Sleep(time.Second * 1)
 }
